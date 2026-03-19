a2911a3def5b642672603d32f9346a2105b3142e
abhirup2000-dev <ghoshabhirup465@gmail.com>
2026-03-19 17:00:02 +0530

    models  update

---
+++ app/models/admin.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    is_admin:{
+      type: String,
+      default: 'admin'
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+
+const AdminModel = mongoose.model('admin', AdminSchema)
+
+
+module.exports = AdminModel
