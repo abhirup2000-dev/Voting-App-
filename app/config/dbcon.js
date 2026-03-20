f12977f6cc73234e7b9f68312a8e2c124944e4c4
suman <suman.sb30@gmail.com>
2026-03-20 11:05:54 +0530

    recent changes

--- app/config/dbcon.js
+++ app/config/dbcon.js
@@ -1,16 +1,26 @@
+
 require("dotenv").config();
+
 const mongoose = require("mongoose");
+
 const DB_URL = process.env.DB_URL;
 
 const DatabaseConnect = async () => {
+  
   try {
+
     const connect = await mongoose.connect(DB_URL);
+
     if (connect) {
+
       console.log("Database connected");
+
     } else {
+
       console.log("Database not connected");
     }
   } catch (err) {
+
     console.log(err);
   }
 };
